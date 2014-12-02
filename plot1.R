364f75ae8b882ec1ec6a21fd269825c45d630186
ezgraphs <ezgraphs@gmail.com>
2014-12-01 21:47:36 -0500

    Adding plots 1 through 4 and images

---
+++ plot1.R
@@ -0,0 +1,26 @@
+library(dplyr)
+
+dataFile <-'data.csv.zip'
+dataUrl  <-'https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip'
+
+if(!file.exists(dataFile)){
+  download.file(url = dataUrl, destfile = dataFile, method = "curl")
+}
+
+data <- read.csv(unzip(dataFile), sep=';', stringsAsFactors=FALSE)
+
+## Subset Data
+df <- data %>% filter(Date== '1/2/2007' | Date== '2/2/2007')
+
+## Format Columns
+df$Global_active_power<-as.numeric(df$Global_active_power)
+df$dateTime<-strptime(paste(df$Date, df$Time), format='%d/%m/%Y %H:%M:%S')
+df$Sub_metering_1<-as.numeric(df$Sub_metering_1)
+df$Sub_metering_2<-as.numeric(df$Sub_metering_2)
+df$Sub_metering_3<-as.numeric(df$Sub_metering_3)
+df$Voltage<-as.numeric(df$Voltage)
+
+## Plot
+png('plot1.png',width = 480, height = 480)
+hist(df$Global_active_power, main='Global Active Power', xlab='Global Active Power (kilowatts)', ylab='Frequency', col='red')
+dev.off()
---
+++ plot4.R
@@ -0,0 +1,46 @@
+library(dplyr)
+
+dataFile <-'data.csv.zip'
+dataUrl  <-'https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip'
+
+if(!file.exists(dataFile)){
+  download.file(url = dataUrl, destfile = dataFile, method = "curl")
+}
+
+data <- read.csv(unzip(dataFile), sep=';', stringsAsFactors=FALSE)
+
+## Subset Data
+df <- data %>% filter(Date== '1/2/2007' | Date== '2/2/2007')
+
+## Format Columns
+df$Global_active_power<-as.numeric(df$Global_active_power)
+df$dateTime<-strptime(paste(df$Date, df$Time), format='%d/%m/%Y %H:%M:%S')
+df$Sub_metering_1<-as.numeric(df$Sub_metering_1)
+df$Sub_metering_2<-as.numeric(df$Sub_metering_2)
+df$Sub_metering_3<-as.numeric(df$Sub_metering_3)
+df$Voltage<-as.numeric(df$Voltage)
+
+## Plot
+png('plot4.png',width = 480, height = 480)
+par(mfrow=c(2,2))
+
+## Plot 1
+plot(df$dateTime, df$Global_active_power, type='l', xlab='', ylab='Global Active Power')
+
+## Plot 2
+plot(df$dateTime, df$Voltage, type='l', xlab='datetime', ylab='Voltage')
+
+## Plot 3
+plot(df$dateTime, df$Sub_metering_1, type='l', xlab='', ylab='Energy sub metering')
+lines(df$dateTime, df$Sub_metering_2, col='red')
+lines(df$dateTime, df$Sub_metering_3, col='blue')
+
+legend( x="topright", 
+        legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), 
+        col=c("black","red","blue"), lwd=1, 
+        lty=c(1,1,1),cex=0.5)
+
+## Plot 4
+with(df, plot(dateTime, Global_reactive_power, type='l', xlab='datetime'))
+
+dev.off()
